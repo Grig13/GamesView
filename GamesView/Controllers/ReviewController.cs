2402f75a5b1fa6fca697436f33246c2110dbee1b
Mazureanu Grigorian <72690365+Grig13@users.noreply.github.com>
2022-05-11 13:57:09 +0300

    created review controller and CRUD operation

---
+++ GamesView/Controllers/ReviewController.cs
@@ -0,0 +1,35 @@
+﻿using GamesView.Models;
+using GamesView.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamesView.Controllers
+{
+    public class ReviewController : Controller
+    {
+        private readonly IReviewRepository _reviewRepository;
+
+        public ReviewController(IReviewRepository reviewRepository)
+        {
+            _reviewRepository = reviewRepository;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Review> reviewsList = _reviewRepository.GetAll();
+            return View(reviewsList);
+        }
+
+        public IActionResult CreateReview()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateNews(Review reviews)
+        {
+            _reviewRepository.Create(reviews);
+            return RedirectToAction("Index");
+        }
+    }
+}
